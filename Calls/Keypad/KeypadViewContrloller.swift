8cea45677a24f104b30bdd2dab7c5bde8dd949bc
Svetoslav Kanchev <svetoslav.Kanchev@infinno.eu>
2021-04-28 12:56:47 +0300

    Code bug fixed

--- Calls/Keypad/KeypadViewContrloller.swift
+++ Calls/Keypad/KeypadViewContrloller.swift
@@ -11,6 +11,7 @@ class KeypadViewController: UIViewController {
     @IBOutlet var keypadButtonViews: [UIView]!
     @IBOutlet var numberLabel: UILabel!
     @IBOutlet var addNumberButton: UIButton!
+    @IBOutlet var deleteButton: UIButton!
    
     let countryCodes = ["389", "84", "590", "31", "880", "688", "886", "964", "974", "266", "359", "508", "229", "501", "995", "44", "267", "48", "372", "264", "230", "680", "970", "340", "596", "33", "687", "968", "41", "269", "232", "358", "299", "594", "502", "238", "676", "258", "251", "670", "226", "382", "225", "376", "509", "500", "32", "591", "690", "966", "261", "378", "53", "90", "221", "345", "36", "62", "691", "375", "56", "960", "213", "216", "381", "507", "243", "855", "965", "92", "86", "7", "377", "852", "351", "260", "30", "265", "237", "240", "242", "503", "298", "268", "256", "850", "872", "60", "853", "98", "976", "39", "371", "967", "370", "387", "234", "49", "248", "250", "94", "971", "290", "58", "994", "689", "963", "962", "228", "692", "380", "239", "992", "506", "223", "996", "77", "421", "43", "599", "95", "34", "55", "386", "246", "856", "81", "678", "353", "27", "65", "61", "254", "681", "45", "57", "686", "972", "679", "354", "977", "40", "350", "672", "537", "47", "257", "236", "993", "998", "385", "93", "291", "420", "674", "683", "66", "255", "82", "961", "212", "356", "227", "673", "220", "975", "262", "253", "249", "597", "675", "224", "593", "355", "64", "682", "1", "231", "263", "374", "373", "244", "973", "505", "252", "677", "235", "297", "233", "685", "222", "91", "20", "245", "52", "379", "218", "54", "595", "423", "504", "46", "51", "63", "284", "352", "598", "241"]
     
@@ -39,7 +40,8 @@ class KeypadViewController: UIViewController {
                     break
                 }
                 for code in countryCodes {
-                    if numberLabel.text!.hasPrefix(code) {
+                    let codePlus = "+" + code
+                    if numberLabel.text! == codePlus {
                         numberLabel.text!.append(" ")
                         break
                     }
@@ -55,7 +57,9 @@ class KeypadViewController: UIViewController {
         
         if numberLabel.text!.count == 1 {
             addNumberButton.isHidden = false
+            deleteButton.isHidden = false
         }
+        changeNumberBackgroundColor(keypadButtonViews[sender.tag])
     }
     
     @IBAction func deleteButton(_ sender: UIButton) {
@@ -65,6 +69,21 @@ class KeypadViewController: UIViewController {
             }
             numberLabel.text!.removeLast()
             addNumberButton.isHidden = numberLabel.text!.isEmpty
+            deleteButton.isHidden = numberLabel.text!.isEmpty
+        }
+    }
+    @IBAction func longPressZero(_ sender: UILongPressGestureRecognizer) {
+        numberLabel.text?.append("+")
+        if numberLabel.text!.count == 1 {
+            addNumberButton.isHidden = false
+            deleteButton.isHidden = false
+        }
+    }
+    
+    func changeNumberBackgroundColor(_ sender: UIView) {
+        UIView.animate(withDuration: 1) {
+            sender.backgroundColor = .systemGray3
+            sender.backgroundColor = .systemGray5
         }
     }
     
